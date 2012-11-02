622ed9652bdf9c909775c135d189760613a17144
Mogale <cgnad@live.de>
2012-11-02 09:54:40 +0100

    DB/Creature: Fix Flying Guards in Capital Cities

---
+++ updates_M/2012_11_02_01_creature_template.sql
@@ -0,0 +1,6 @@
+-- Flying Guards in Capitals should fly, not fall
+UPDATE `creature_template` SET `InhabitType`=4 WHERE `entry`=51383; -- Ironforge Gryphon Rider
+UPDATE `creature_template` SET `InhabitType`=4 WHERE `entry`=51346; -- Orgrimmar Wind Rider
+UPDATE `creature_template` SET `InhabitType`=4 WHERE `entry`=51372; -- Thunder Bluff Wind Rider
+UPDATE `creature_template` SET `InhabitType`=4 WHERE `entry`=51384; -- Forsaken Bat
+UPDATE `creature_template` SET `InhabitType`=4 WHERE `entry`=51371; -- Darnassus Hippogryph Rider
